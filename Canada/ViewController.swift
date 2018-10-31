f89fa7bbef063418d239f97fb0925cc7e3f156db
dev <tahmed36@sy-t3078-17.local>
2018-10-31 15:34:37 -0400

    Initial Commit

---
+++ Canada/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Canada
+//
+//  Created by Thaharim Ahmed on 2018-10-31.
+//  Copyright © 2018 Thaharim Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CanadaTests/CanadaTests.swift
@@ -0,0 +1,36 @@
+//
+//  CanadaTests.swift
+//  CanadaTests
+//
+//  Created by Thaharim Ahmed on 2018-10-31.
+//  Copyright © 2018 Thaharim Ahmed. All rights reserved.
+//
+
+import XCTest
+@testable import Canada
+
+class CanadaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
